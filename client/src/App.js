3a106a37f070f0a4becbac2b27d167072be41fe2
Lê Khánh <lekhanh240895@gmail.com>
2023-03-12 18:51:25 +0700

    Fixed socket host

--- client/src/App.js
+++ client/src/App.js
@@ -1,153 +1,159 @@
-import { Fragment, useEffect } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Fragment, useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
-import { publicRoutes, privateRoutes } from "~/routes/index";
-import DefaultLayout from "~/layouts/DefaultLayout";
-import GetAppButton from "./components/BottomContainer/BottomContainer";
-import EditProfileModal from "./components/modals/EditProfileModal";
-import { useDispatch, useSelector } from "react-redux";
+import { publicRoutes, privateRoutes } from '~/routes/index'
+import DefaultLayout from '~/layouts/DefaultLayout'
+import GetAppButton from './components/BottomContainer/BottomContainer'
+import EditProfileModal from './components/modals/EditProfileModal'
+import { useDispatch, useSelector } from 'react-redux'
 import {
-  appSelector,
-  authSelector,
-  editModalSelector,
-  loginModalSelector,
-  videosSelector,
-} from "./redux/selectors";
-import { getUsers } from "./redux/slices/usersSlice";
-import appSlice from "./redux/slices/appSlice";
-import videosSlice, { getVideos } from "./redux/slices/videosSlice";
-import LoginModal from "./components/modals/LoginModal";
-import PrivateOutlet from "./components/PrivateRouteOutlet";
-import { getMe } from "./redux/slices/authSlice";
-import Spinner from "./components/Spinner/Spinner";
-import { io } from "socket.io-client";
-import DeleteVideoModal from "./components/modals/DeleteVideoModal";
+    appSelector,
+    authSelector,
+    editModalSelector,
+    loginModalSelector,
+    videosSelector,
+} from './redux/selectors'
+import { getUsers } from './redux/slices/usersSlice'
+import appSlice from './redux/slices/appSlice'
+import videosSlice, { getVideos } from './redux/slices/videosSlice'
+import LoginModal from './components/modals/LoginModal'
+import PrivateOutlet from './components/PrivateRouteOutlet'
+import { getMe } from './redux/slices/authSlice'
+import Spinner from './components/Spinner/Spinner'
+import { io } from 'socket.io-client'
+import DeleteVideoModal from './components/modals/DeleteVideoModal'
 
 function App() {
-  const { isLoading, videos } = useSelector(videosSelector);
-  const { isEditModalShow } = useSelector(editModalSelector);
-  const { isLoginModalShow } = useSelector(loginModalSelector);
-  const dispatch = useDispatch();
-  const { currentUser } = useSelector(authSelector);
-  const { socket, isDeleteModalShow } = useSelector(appSelector);
-  const location = useLocation();
+    const { isLoading, videos } = useSelector(videosSelector)
+    const { isEditModalShow } = useSelector(editModalSelector)
+    const { isLoginModalShow } = useSelector(loginModalSelector)
+    const dispatch = useDispatch()
+    const { currentUser } = useSelector(authSelector)
+    const { socket, isDeleteModalShow } = useSelector(appSelector)
+    const location = useLocation()
 
-  useEffect(() => {
-    const url = process.env.REACT_APP_SOCKET_URL;
-    const socket = io(url);
-    dispatch(appSlice.actions.setSocket(socket));
-  }, [dispatch]);
+    useEffect(() => {
+        const url = process.env.REACT_APP_SOCKET_URL
+        const socket = io('https://tiktok-socket.onrender.com')
+        dispatch(appSlice.actions.setSocket(socket))
+    }, [dispatch])
 
-  useEffect(() => {
-    if (currentUser) socket?.emit("addUser", currentUser._id);
-    socket?.on("getUsers", (users) => {
-      dispatch(appSlice.actions.setOnlineUsers(users));
-    });
-  }, [socket, dispatch, currentUser]);
+    useEffect(() => {
+        if (currentUser) socket?.emit('addUser', currentUser._id)
+        socket?.on('getUsers', (users) => {
+            dispatch(appSlice.actions.setOnlineUsers(users))
+        })
+    }, [socket, dispatch, currentUser])
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    // Fetch users
-    dispatch(getUsers());
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        // Fetch users
+        dispatch(getUsers())
 
-    // Fetch videos
-    dispatch(getVideos());
+        // Fetch videos
+        dispatch(getVideos())
 
-    if (token) dispatch(getMe());
+        if (token) dispatch(getMe())
 
-    return () => {
-      dispatch(videosSlice.actions.reset());
-    };
-  }, [dispatch]);
+        return () => {
+            dispatch(videosSlice.actions.reset())
+        }
+    }, [dispatch])
 
-  function unique(arr) {
-    var newArr = [];
-    for (var i = 0; i < arr.length; i++) {
-      if (!newArr.includes(arr[i])) {
-        newArr.push(arr[i]);
-      }
+    function unique(arr) {
+        var newArr = []
+        for (var i = 0; i < arr.length; i++) {
+            if (!newArr.includes(arr[i])) {
+                newArr.push(arr[i])
+            }
+        }
+        return newArr
     }
-    return newArr;
-  }
 
-  useEffect(() => {
-    (async () => {
-      if (videos?.length > 0) {
-        const tags = videos.reduce((cur, video) => {
-          return cur.concat(video.tags?.map((tag) => tag));
-        }, []);
+    useEffect(() => {
+        ;(async () => {
+            if (videos?.length > 0) {
+                const tags = videos.reduce((cur, video) => {
+                    return cur.concat(video.tags?.map((tag) => tag))
+                }, [])
 
-        const filterTags = unique(tags);
-        const musics = videos.reduce((cur, video) => {
-          return cur.includes(video.music) ? cur : [...cur, video.music];
-        }, []);
+                const filterTags = unique(tags)
+                const musics = videos.reduce((cur, video) => {
+                    return cur.includes(video.music)
+                        ? cur
+                        : [...cur, video.music]
+                }, [])
 
-        dispatch(appSlice.actions.setTags(filterTags));
-        dispatch(appSlice.actions.setMusics(musics));
-      }
-    })();
-  }, [dispatch, videos]);
+                dispatch(appSlice.actions.setTags(filterTags))
+                dispatch(appSlice.actions.setMusics(musics))
+            }
+        })()
+    }, [dispatch, videos])
 
-  if (isLoading) return <Spinner />;
+    if (isLoading) return <Spinner />
 
-  return (
-    <div className="App">
-      {/* <Routes location={background || location}> */}
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          let Layout = DefaultLayout;
-          const Page = route.component;
+    return (
+        <div className="App">
+            {/* <Routes location={background || location}> */}
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    let Layout = DefaultLayout
+                    const Page = route.component
 
-          if (route.layout) {
-            Layout = route.layout;
-          } else if (route.layout === null) {
-            Layout = Fragment;
-          }
+                    if (route.layout) {
+                        Layout = route.layout
+                    } else if (route.layout === null) {
+                        Layout = Fragment
+                    }
 
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          );
-        })}
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    )
+                })}
 
-        {privateRoutes.map((route, index) => {
-          let Layout = DefaultLayout;
-          const Page = route.component;
+                {privateRoutes.map((route, index) => {
+                    let Layout = DefaultLayout
+                    const Page = route.component
 
-          if (route.layout) {
-            Layout = route.layout;
-          } else if (route.layout === null) {
-            Layout = Fragment;
-          }
+                    if (route.layout) {
+                        Layout = route.layout
+                    } else if (route.layout === null) {
+                        Layout = Fragment
+                    }
 
-          return (
-            <Route path={route.path} key={index} element={<PrivateOutlet />}>
-              <Route
-                path={route.path}
-                element={
-                  <Layout>
-                    <Page />
-                  </Layout>
-                }
-              />
-            </Route>
-          );
-        })}
+                    return (
+                        <Route
+                            path={route.path}
+                            key={index}
+                            element={<PrivateOutlet />}
+                        >
+                            <Route
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        </Route>
+                    )
+                })}
 
-        {/* <Route
+                {/* <Route
                     path={`@:username/video/:videoID`}
                     element={<VideoModal />}
                 /> */}
-      </Routes>
+            </Routes>
 
-      {/* {background && (
+            {/* {background && (
                 <Routes>
                     <Route
                         path={`@:username/video/:videoID`}
@@ -156,12 +162,12 @@ function App() {
                 </Routes>
             )} */}
 
-      {location.pathname !== "/messages" && <GetAppButton />}
-      {isEditModalShow && <EditProfileModal />}
-      {isLoginModalShow && <LoginModal />}
-      {isDeleteModalShow && <DeleteVideoModal />}
-    </div>
-  );
+            {location.pathname !== '/messages' && <GetAppButton />}
+            {isEditModalShow && <EditProfileModal />}
+            {isLoginModalShow && <LoginModal />}
+            {isDeleteModalShow && <DeleteVideoModal />}
+        </div>
+    )
 }
 
-export default App;
+export default App
